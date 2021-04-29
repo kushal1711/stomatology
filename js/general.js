df70842ee427cdb274ea29c74c446c16d69c245d
kushal1711 <77674349+kushal1711@users.noreply.github.com>
2021-04-29 13:20:34 +0530

    Add files via upload

---
+++ js/general.js
@@ -0,0 +1,64 @@
+// toggle menu js 
+
+(function($){
+    $('.nav-toggle').click(function(e){
+        e.preventDefault();
+        $('.nav-toggle').toggleClass('active');
+        $('.nav-menu').toggleClass('active');
+    })
+})(jQuery);
+
+// Owl slider js 
+
+$(document).ready(function(){
+//home banner slider js
+
+$('.hmbanner').owlCarousel({
+   loop:true,
+   nav :true,
+   navText :["<i class='fas fa-long-arrow-alt-left'></i>","<i class='fas fa-long-arrow-alt-right'></i>"],
+   items:1,
+   dots:false
+})
+// Services slider js 
+
+$('.services').owlCarousel({
+    loop:true,
+    nav:true,
+    dots:false,
+    arrow:false,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:3
+        },
+        1000:{
+            items:4
+        }
+    }
+})
+
+//offer slider js 
+
+$('.offer').owlCarousel({
+    loop:true,
+    nav:true,
+    dots:false,
+    arrow:false,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:3
+        },
+        1000:{
+            items:5
+        }
+    }
+})
+});            
+
+
