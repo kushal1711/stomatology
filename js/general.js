beaacea637eef93d0f82655ea0481c10599fa235
kushal1711 <77674349+kushal1711@users.noreply.github.com>
2021-04-29 14:12:05 +0530

    Update general.js

--- js/general.js
+++ js/general.js
@@ -47,12 +47,13 @@ $('.offer').owlCarousel({
     nav:false,
     dots:false,
     arrow:false,
+    margin:10,
     responsive:{
         0:{
             items:1
         },
         600:{
-            items:3
+            items:2
         },
         1000:{
             items:5
@@ -61,4 +62,21 @@ $('.offer').owlCarousel({
 })
 });            
 
+// Magnific Popup js 
 
+$(document).ready(function() {
+	$('.mfpgallery').magnificPopup({
+		delegate: 'a',
+		type: 'image',
+		tLoading: 'Loading image #%curr%...',
+		mainClass: 'mfp-img-mobile',
+		gallery: {
+			enabled: true,
+			navigateByImgClick: true,
+			preload: [0,1] // Will preload 0 - before current, and 1 after the current image
+		},
+		image: {
+			tError: '<a href="%url%">The image #%curr%</a> could not be loaded.',
+		}
+	});
+});
